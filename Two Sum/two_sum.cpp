ffca98488ea34517b3f9d7cb6f673cd1d33b6306
shabbir0623 <shaabbirzaman@gmail.com>
2020-10-24 07:37:06 +0600

    Leetcode (Easy)

---
+++ Two Sum/two_sum.cpp
@@ -0,0 +1,34 @@
+/*
+author: shabbir0623
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+
+class Solution {
+public:
+    vi twoSum(vi& nums, int target) {
+        for(vi::iterator i = nums.begin(); i != nums.end(); i++){
+            vi::iterator res = find(i+1, nums.end(), target - *i);
+            if(res != nums.end()) 
+            	return (vi){(int)distance(nums.begin(), i), (int)distance(nums.begin(), res)};
+		}
+       	return (vi){};
+    }
+};
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+    Solution soln;
+    vi nums = (vi){3, 2, 4};
+    int target = 6;
+    
+    
+
+	vi result = soln.twoSum(nums, target);
+	if(!result.empty()) 
+		cout << result[0] << ", " << result[1] << endl;
+	return 0;
+}
